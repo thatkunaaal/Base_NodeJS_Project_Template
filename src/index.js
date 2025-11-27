6e2c6116c356e5ad98d0d2fde6e0d2b9ec4ea61c
thatkunaaal <thatkunaal@gmail.com>
2025-11-27 07:32:22 +0530

    Added: winston logger

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {serverConfig} = require('./config') ;
+const {ServerConfig, Logger} = require('./config') ;
 
 const app = express();
 const apiRoutes = require('./routes')
@@ -8,6 +8,7 @@ const apiRoutes = require('./routes')
 app.use('/api',apiRoutes);
 
 
-app.listen(serverConfig.PORT,()=>{
-    console.log(`Server is up and running on port: ${serverConfig.PORT}`)
+app.listen(ServerConfig.PORT,()=>{
+    console.log(`Server is up and running on port: ${ServerConfig.PORT}`)
+    Logger.info({msg:"Hello from the server"},{error: "I under the water!!"})
 })
